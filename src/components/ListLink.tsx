2e6c0185f8a6a18900208d542ca58507c39f7ee2
Vidit Khandelwal <viditk17@gmail.com>
2023-02-23 19:52:57 -0500

    fixed link issue

--- src/components/ListLink.tsx
+++ src/components/ListLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ListLink = (props: {href: string, children: any}) => {
   return (
     <Link
-      href="https://snapshot.viditkhandelwal.com"
+      href={props.href}
       rel="noreferrer noopener"
       className="flex flex-col lg:flex-row lg:space-x-4 justify-between items-start lg:items-center p-2 rounded-xl hover:bg-neutral-500 dark:hover:bg-neutral-100 hover:bg-opacity-10 dark:hover:bg-opacity-10 w-11/12 lg:w-3/4"
     >
